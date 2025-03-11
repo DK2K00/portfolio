9cdef50f7f1eb8b6b091a9f610c5ab6edd44dc3a
Danush Kumar <danushkumar27@gmail.com>
2025-03-11 11:44:43 -0500

    Auto-generated commit

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -2,13 +2,13 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: 'C:/Program Files/Git/portfolio/',
+  baseHref: 'portfolio/',
   locale: undefined,
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 19195, hash: '0bd5a15cef69074d321d064c731b9b9c01641d69a9e43e56dd73bd6b12d161ea', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 19463, hash: 'e317b60e90e44b3cf3760c9564e17b8c67661996b5d7521913ba5d049b2d1965', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 19174, hash: 'd3a3e68097f1b1876362dce92fa29b3723d0cde9562b8cbe7fab1a7198cbd801', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 19442, hash: '4afd76d3a6df59902cfac874fa4d8409d487f06e8cedec0efb417b82b3691806', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-XP3E2BSF.css': {size: 120, hash: 'tQV8K5IPxxE', text: () => import('./assets-chunks/styles-XP3E2BSF_css.mjs').then(m => m.default)}
   },
 };
